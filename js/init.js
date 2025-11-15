408ce948c31018bd6b7f29b91c7b736b9a156397
CH33ZELOUIZ <yampoljeffrey@gmail.com>
2025-11-15 13:43:13 -0800

    initial

--- js/init.js
+++ js/init.js
@@ -194,38 +194,3 @@ function collapse() {
       moreless.innerHTML = "less"
    }
 }
-
-/*----------------------------------------------------*/
-/*	Hide/Show
-------------------------------------------------------*/
-window.addEventListener('hashchange', () => {
-   hide();
-});
-
-function hide() {
-   if (window.location.href.indexOf("btw285") > -1) {
-      document.getElementById('about').style.display = "none";
-      document.getElementById('resume').style.display = "none";
-      document.getElementById('projects').style.display = "none";
-      document.getElementById('designReport').style.display = "none";
-      document.getElementById('btw285').style.display = "block";
-      document.getElementById('btw2852').style.display = "block";
-      document.getElementById('portfolio').style.display = "block";
-   } else if (window.location.href.indexOf("designReport") > -1) {
-      document.getElementById('about').style.display = "none";
-      document.getElementById('portfolio').style.display = "none";
-      document.getElementById('resume').style.display = "none";
-      document.getElementById('projects').style.display = "none";
-      document.getElementById('designReport').style.display = "block";
-      document.getElementById('btw285').style.display = "none";
-      document.getElementById('btw2852').style.display = "none";
-   } else {
-      document.getElementById('about').style.display = "block";
-      document.getElementById('resume').style.display = "block";
-      document.getElementById('projects').style.display = "block";
-      document.getElementById('designReport').style.display = "none";
-      document.getElementById('btw285').style.display = "none";
-      document.getElementById('btw2852').style.display = "none";
-      document.getElementById('portfolio').style.display = "block";
-   }
-}
